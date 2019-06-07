5db74b36e8a015c572cb86b93769eec13a5de1fc
Mayank Morya <moryamayank9@gmail.com>
2019-06-07 18:30:28 +0530

    SB-12731 : Org service impl for default success responses (#2)
    * org-related-api added
    
    * Issue #SB-12812  Docker and Docker.Build file changed renamed learner to user-service and dependency removed for common-util and request-validator
    
    * serperate module added for org-service
    
    * removed commom-utils dependency
    
    * Delete .classpath
    
    * Delete org.eclipse.jdt.core.prefs
    
    * Delete .project
    
    * pom correction
    
    * name correction
    
    * project arch changed
    
    * project name correction
    
    * submodule correction
    
    * Delete .project
    
    * Delete .classpath
    
    * Delete org.eclipse.jdt.core.prefs
    
    * service name correction

---
+++ user-org-service/app/controllers/orgmanagement/OrgTypeController.java
@@ -0,0 +1,39 @@
+package controllers.orgmanagement;
+
+import controllers.BaseController;
+import java.util.concurrent.CompletionStage;
+import play.mvc.Result;
+
+/**
+ * This Controller is dedicated to organisation type creation , update , listing . CompletionStage:
+ * A stage of a possibly asynchronous computation, that performs an action or computes a value when
+ * another CompletionStage completes
+ */
+public class OrgTypeController extends BaseController {
+
+  /**
+   * This method is used for creating organisations type,
+   *
+   * @return Object of created orgType
+   */
+  public CompletionStage<Result> createOrgType() {
+    return handelRequest();
+  }
+  /**
+   * This method is used for updating organisations type,
+   *
+   * @return response code for failure or success
+   */
+  public CompletionStage<Result> updateOrgType() {
+    return handelRequest();
+  }
+
+  /**
+   * This method is used for listing organisations type,
+   *
+   * @return list of Object of orgType
+   */
+  public CompletionStage<Result> listOrgType() {
+    return handelRequest();
+  }
+}
