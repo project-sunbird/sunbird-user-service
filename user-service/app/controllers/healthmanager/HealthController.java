0a4b6f2cdcfa2435e2ba12f1ca6f46faba1fbefa
anmol2302 <anmolgupta23021997@gmail.com>
2019-06-07 17:48:53 +0530

    Issue #SB-12934 added health check controller and test cases

---
+++ user-service/app/controllers/healthmanager/HealthController.java
@@ -0,0 +1,39 @@
+package controllers.healthmanager;
+
+import controllers.BaseController;
+import play.mvc.Result;
+
+import java.util.concurrent.CompletionStage;
+
+
+/**
+ * This controller class will responsible to check health of the services.
+ *
+ * @author Anmol
+ */
+public class HealthController extends BaseController {
+
+
+    /**
+     * This action method is responsible for checking Health.
+     *
+     * @return a CompletableFuture of success response
+     */
+
+    public CompletionStage<Result> getHealth() {
+        return handelRequest();
+    }
+
+
+    /**
+     * This action method is responsible to check user-service health
+     *
+     * @return a CompletableFuture of success response
+     */
+    public CompletionStage<Result> getUserServiceHealth(String health) {
+        return handelRequest();
+    }
+
+
+}
+
