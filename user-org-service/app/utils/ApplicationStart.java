9eabbb3448df91633ba8c4e49d2eb30198a1fce0
manzarul Haque <manzarul07@gmail.com>
2019-06-11 16:41:47 +0530

    Issue #SB-12996 Add startup class (#6)

---
+++ user-org-service/app/utils/ApplicationStart.java
@@ -0,0 +1,34 @@
+package utils;
+
+import java.util.concurrent.CompletableFuture;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import play.api.Environment;
+import play.api.inject.ApplicationLifecycle;
+
+/**
+ * This class will be called after on application startup.
+ * only one instance of this class will be created.
+ * StartModule class has responsibility to eager load this class.
+ * @author manzarul
+ *
+ */
+@Singleton
+public class ApplicationStart {
+	  /**
+	   * All one time initialization which required during server startup will fall here.
+	   * @param lifecycle ApplicationLifecycle
+	   * @param environment Environment
+	   */
+	  @Inject
+	  public ApplicationStart(ApplicationLifecycle lifecycle, Environment environment) {
+	    // Shut-down hook
+	    lifecycle.addStopHook(
+	        () -> {
+	          return CompletableFuture.completedFuture(null);
+	        });
+	  }
+
+}
