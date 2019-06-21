5489963a22a66512745ba58af4f2c44b1a5e2ac2
amit <amit.kumar@tarento.com>
2019-06-21 15:52:56 +0530

    Issue # SC-1084 feat: modified user-org-actor pom.xml file

--- actor-service/src/main/java/org/sunbird/actor/core/BaseActor.java
+++ actor-service/src/main/java/org/sunbird/actor/core/BaseActor.java
@@ -63,44 +63,7 @@ public abstract class BaseActor extends UntypedAbstractActor {
         sender().tell(exception, self());
     }
 
-    /**
-     * TODO : Need to remove in future
-     */
-    private SupervisorStrategy strategy = new OneForOneStrategy(false, DeciderBuilder.
-            match(ArithmeticException.class, e -> {
-                return SupervisorStrategy.restart();
-            }).
-            match(ArithmeticException.class, e -> {
-                notifyConsumerFailure("operation", e);
-                return SupervisorStrategy.stop();
-            }).
-            match(Throwable.class, e -> {
-                notifyConsumerFailure("operation", e);
-                return SupervisorStrategy.stop();
-            }).build());
-
-    @Override
-    public SupervisorStrategy supervisorStrategy() {
-        return strategy;
-    }
-
-    private void notifyConsumerFailure(String operation, Throwable failure) {
-        ActorRef actorRef = ActorCache.getActorRef(operation);
-        if (actorRef != null) {
-            actorRef.tell(new Status.Failure(failure), self());
-        }
-    }
-
-    protected ActorRef getChildActor(Class<? extends BaseActor> actor, String dispatcher, ActorContext context){
-        Props props;
-        if (StringUtils.isNotBlank(dispatcher)) {
-            props = Props.create(actor).withDispatcher(dispatcher);
-        } else {
-            props = Props.create(actor);
-        }
-        return context.actorOf(FromConfig.getInstance().props(props), actor.getSimpleName());
-    }
-
+    
     protected String getLocalizedMessage(String key, Locale locale){
         return localizer.getMessage(key, locale);
     }
