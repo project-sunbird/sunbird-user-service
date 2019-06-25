6646be183dda47585c55324ede11744ced8114f5
manzarul <manzarul07@gmail.com>
2019-06-25 16:23:18 +0530

    Issue #SC-1090 feat: adding es module into user org service

---
+++ user-org-es-util/src/main/java/org/sunbird/helper/EsConstant.java
@@ -0,0 +1,33 @@
+package org.sunbird.helper;
+
+public class EsConstant {
+  public static final String SUNBIRD_ES_CLUSTER = "sunbird_es_cluster";
+  public static final String SUNBIRD_ES_IP = "sunbird_es_host";
+  public static final String SUNBIRD_ES_PORT = "sunbird_es_host";
+  public static final String EXISTS = "exists";
+  public static final String NOT_EXISTS = "not_exists";
+  public static final String FILTERS = "filters";
+  public static final String GROUP_QUERY = "groupQuery";
+  public static final String SOFT_CONSTRAINTS = "softConstraints";
+  public static final String LIMIT = "limit";
+  public static final String OFFSET = "offset";
+  public static final String QUERY = "query";
+  public static final String QUERY_FIELDS = "queryFields";
+  public static final String FACETS = "facets";
+  public static final String FIELDS = "fields";
+  public static final String SORT_BY = "sort_by";
+  public static final String CONTENT = "content";
+  public static final String COUNT = "count";
+  public static final String DATE_HISTOGRAM = "DATE_HISTOGRAM";
+  public static final String NAME = "name";
+  public static final String SUNBIRD_ES_CHANNEL = "es.channel.name";
+  public static final String CHANNEL = "channel";
+  public static final String TERM = "term";
+  public static final String ORDER = "order";
+  public static final String ID = "id";
+  public static final String IDENTIFIER = "identifier";
+  public static final String USER_INDEX_NAME = "user";
+  public static final String ES_METRICS_PORT = "es_metrics_port";
+  public static final String ES_URL = "es_search_url";
+  public static final String RESPONSE = "response";
+}
