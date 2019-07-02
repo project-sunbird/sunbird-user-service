9656cdcbf5442aac9b61d8cf688f1c7a966e2d1a
anmol2302 <anmolgupta23021997@gmail.com>
2019-07-02 12:03:33 +0530

    Issue#SC-1109 elastic search time out problem fixed

--- user-org-actor/src/main/java/org/sunbird/user/dao/IUserESDao.java
+++ user-org-actor/src/main/java/org/sunbird/user/dao/IUserESDao.java
@@ -9,7 +9,21 @@ import java.util.Map;
  * @author anmolgupta
  */
 public interface IUserESDao {
+
+    /**
+     * this method will be used to get user by id from elastic search.
+     * @param userId
+     * @return
+     * @throws BaseException
+     */
     public Response getUserById(String userId) throws BaseException;
 
+
+    /**
+     * this method will be used to search user from elasticsearch
+     * @param request
+     * @return
+     * @throws BaseException
+     */
     public Response searchUser(Map<String, Object> request) throws BaseException;
 }
