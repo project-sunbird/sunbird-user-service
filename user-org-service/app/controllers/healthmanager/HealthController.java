5808318fa3141913532574c16c6a1622edcad370
Rajesh Rathnam <indrajra@users.noreply.github.com>
2019-07-05 17:35:47 +0530

    Update HealthController.java

--- user-org-service/app/controllers/healthmanager/HealthController.java
+++ user-org-service/app/controllers/healthmanager/HealthController.java
@@ -12,7 +12,7 @@ import play.mvc.Results;
  * @author Anmol
  */
 public class HealthController extends BaseController {
-
+  // Service name must be "service" for the devops monitoring.
   private static final String service = "service";
 
   /**
