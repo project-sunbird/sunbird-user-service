87b6680a83408341890df90f7c986a9060ba678a
anmol2302 <anmolgupta23021997@gmail.com>
2019-07-22 11:44:25 +0530

    Issue#SC-1088 Validation of user create and Log API added with the caching of schema instances

--- user-org-service/app/utils/validator/IModelValidator.java
+++ user-org-service/app/utils/validator/IModelValidator.java
@@ -1,6 +1,5 @@
 package utils.validator;
 
-
 /**
  * Each validator class needs to extends this interface class
  *
@@ -8,6 +7,9 @@ package utils.validator;
  */
 @FunctionalInterface
 public interface IModelValidator {
-  /** this method will be implemented in every subclass to validate the play request */
+  /**
+   * this method will be implemented in every subclass to validate the play request if schema fails
+   * to validate it will throw the org.everit.json.schema.ValidationException
+   */
   void validate();
 }
